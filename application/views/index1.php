0b3ca19590acf518ca93826278f36f403cc7321d
leo <leoalopes207@gmail.com>
2017-04-06 21:58:59 -0300

    semantic-ui

---
+++ application/views/index1.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Sobre</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>/assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?php echo base_url(); ?>/assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="blue darken-4">
+    <div class="row">
+      <div class="nav-wrapper col s10 offset-s1">
+        <a class="brand-logo white-text"><b>Plataforma</b></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="conta" class="white-text">Entrar</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="section no-pad-bot white" style="margin-top:3%">
+    <div class="container">
+      <h2 class="header center black-text"><b>Alguma coisa</b></h2>
+      <p class="light center">
+        Aqui vai ter mais alguma coisa dizendo algo também
+      </p>
+      <div class="center-btn">
+        <br><a href="conta" class="waves-effect waves-light btn blue darken-3 white-text">Entrar</a><br><br>
+      </div>
+    </div>
+  </div>
+
+  <br><br><div class="container row">
+    <h3 class="header center black-text"><b>Aqui é alguma coisa que vai falar sobre o que o meu site faz, só preciso descobrir isso também</b></h3><br>
+    <div class="col s4">
+      <div class="center-icon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="material-icons large grey-text text-darken-3">assignment</i></div>
+      <h5 class="header center black-text">Algo aqui</h5>
+      <p style="text-align: justify">Um texto explicando o que o algo que eu coloquei ali em cima quer dizer e como isso vai funcionar dentro desse bagulho aqui.</p>
+    </div>
+    <div class="col s4">
+      <div class="center-icon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="material-icons large yellow-text text-darken-3">mode_edit</i></div>
+      <h5 class="header center black-text">Outro algo</h5>
+      <p style="text-align: justify">Um texto explicando o que o outro algo que eu coloquei ali em cima quer dizer e como isso vai funcionar dentro desse bagulho aqui.</p>
+    </div>
+    <div class="col s4">
+      <div class="center-icon">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="material-icons large blue-text text-darken-3">settings</i></div>
+      <h5 class="header center black-text">Mais um algo</h5>
+      <p style="text-align: justify">Um texto explicando o que o mais um algo que eu coloquei ali em cima quer dizer e como isso vai funcionar dentro desse bagulho aqui.</p>
+    </div>
+    <br><br>
+  </div><br><br>
+
+  <footer class="page-footer grey lighten-3">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5>Alguma coisa</h5>
+          <p class="black-text">Aqui eu acho que é alguma coisa também, não sei.</p>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright grey lighten-2 black-text">
+    <div class="container">
+      Leonardo de Araujo Lopes
+    </div>
+    </div>
+  </footer>
+
+  <!--  Scripts-->
+  <script src="<?php echo base_url(); ?>/assets/js/jquery-3.1.1.js"></script>
+  <script src="<?php echo base_url(); ?>/assets/js/materialize.js"></script>
+  <script src="<?php echo base_url(); ?>/assets/js/init.js"></script>
+
+  </body>
+</html>
